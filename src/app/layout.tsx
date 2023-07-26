46c85eea4e4c6c4f0f4a40990c7d5f02598e568f
Ethan Morris <pirates.postbox-0t@icloud.com>
2023-07-25 20:43:19 -0500

    feat: Add support for dark mode using the `class` variant in Tailwind CSS
    feat: Add `components.json` file to configure UI components with Tailwind CSS
    
    feat: Add new dependencies for UI components and fonts
    
    feat: Add `profile.png` image to the public directory
    
    refactor: Move global styles from `src/app/globals.css` to `src/styles/globals.css`
    
    refactor: Update import path for global styles in `src/app/layout.tsx`
    
    feat: Add `mode-toggle.tsx` component for theme mode toggling
    
    feat: Add `theme-provider.tsx` component for theme provider
    
    feat: Add `button.tsx` component for UI buttons
    
    feat: Add `dropdown-menu.tsx` component for dropdown menus
    
    feat: Add `sheet.tsx` component for modal sheets
    
    feat: Add `utils.ts` file for utility functions
    
    refactor: Update import path for global styles in `src/app/page.tsx`
    
    refactor: Update import path for global styles in `src/app/layout.tsx`
    
    refactor: Update import path for global styles in `src/app/page.tsx`
    
    refactor: Update import path for global styles in `src/app/layout.tsx`
    
    refactor: Update import path for global styles in `src/app/page.tsx`
    
    refactor: Update import path for global styles in `src/app/layout.tsx`
    
    refactor: Update import path for global styles in `src/app/page.tsx`
    
    refactor: Update import path for global styles in `src/app/layout.tsx`
    
    refactor: Update import path for global styles in `src/app/page.tsx`
    
    refactor: Update import path for global styles in `src/app/layout.tsx`
    
    refactor: Update import path for global styles in `src/app/page.tsx`
    
    refactor: Update import path for global styles in `src/app/layout.tsx`
    
    refactor: Update import path for global styles in `src/app/page.tsx`
    
    refactor: Update import path for global styles in `src/app/layout.tsx`
    
    refactor: Update import path for global styles in `src/app/page.tsx`
    
    refactor: Update import path for global styles in `src/app/layout.tsx`
    
    refactor: Update import path for global styles in `src/app/page.tsx`
    
    refactor: Update import path for global styles in `src/app/layout.tsx`
    
    refactor: Update import path for global styles in `src/app/page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
-import './globals.css';
+import '../styles/globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Manrope } from 'next/font/google';
+import { ThemeProvider } from '@/components/theme-provider';
 
-const inter = Inter({ subsets: ['latin'] });
+const manrope = Manrope({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
 	title: 'ethanmorr.is',
@@ -12,7 +13,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang='en'>
-			<body className={inter.className}>{children}</body>
+			<body className={manrope.className}>
+				<ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+					{children}
+				</ThemeProvider>
+			</body>
 		</html>
 	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from '@/components/mode-toggle';
 import Image from 'next/image';
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
 				<p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
 					Welcome to the Thunderdome.
 				</p>
+				<ModeToggle />
 				<div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none'>
 					<a
 						className='pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0'
@@ -15,11 +17,11 @@ export default function Home() {
 						rel='noopener noreferrer'>
 						By{' '}
 						<Image
-							src='/vercel.svg'
+							src='/profile.png'
 							alt='Vercel Logo'
-							className='dark:invert'
-							width={100}
-							height={24}
+							className='rounded-full'
+							width={50}
+							height={50}
 							priority
 						/>
 					</a>
